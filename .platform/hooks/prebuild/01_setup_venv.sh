ddadf102067854328c0866afe856700acb57add9
EC2 Default User <ec2-user@ip-172-31-32-115.eu-west-1.compute.internal>
2024-12-03 19:54:35 +0000

    Add platform and EB configuration for deployment

---
+++ .platform/hooks/prebuild/01_setup_venv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Create a virtual environment if it doesn't exist
+if [ ! -d "/var/app/staging/venv" ]; then
+    python3 -m venv /var/app/staging/venv
+fi
+
+# Install dependencies
+/var/app/staging/venv/bin/pip install -r /var/app/staging/requirements.txt
+
