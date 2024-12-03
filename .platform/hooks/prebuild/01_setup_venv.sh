9ad302c5a41a4da449e03ca7d6eacb79136bfd88
EC2 Default User <ec2-user@ip-172-31-32-115.eu-west-1.compute.internal>
2024-12-03 21:55:57 +0000

    Update venv setup to bypass yum install

--- .platform/hooks/prebuild/01_setup_venv.sh
+++ .platform/hooks/prebuild/01_setup_venv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-# Create a virtual environment if it doesn't exist
+# Create virtual environment if it doesn't exist
 if [ ! -d "/var/app/staging/venv" ]; then
     python3 -m venv /var/app/staging/venv
 fi
 
 # Install dependencies
+/var/app/staging/venv/bin/pip install --upgrade pip
 /var/app/staging/venv/bin/pip install -r /var/app/staging/requirements.txt
