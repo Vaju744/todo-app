79bc0f62a4fcd0765060ddf03716904dae1946b5
Vaju744 <x23325666@student.ncirl.ie>
2024-12-11 23:40:20 +0000

    changes

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-
+echo "Stopping Gunicorn and Nginx services..."
+sudo systemctl stop gunicorn
+sudo systemctl stop nginx
+ 
+#To  Define the dynamic deployment directory
+DEPLOYMENT_DIR=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
+ 
+# Clean up old files
+echo "Cleaning up old files in /home/ec2-user/todo..."
+sudo rm -rf /home/ec2-user/todo/*
+ 
+# Copy new files from the dynamic deployment directory
+echo "Copying new files from $DEPLOYMENT_DIR to /home/ec2-user/todo..."
+sudo cp -r $DEPLOYMENT_DIR/* /home/ec2-user/todo
+ 
+# Set proper permissions to access directoryies and files
+echo "Setting ownership and permissions for /home/ec2-user/todo..."
+sudo chown -R ec2-user:ec2-user /home/ec2-user/todo
+sudo chmod -R 755 /home/ec2-user/todo
 sudo yum update -y
 
 sudo yum install -y python3 python3-pip
@@ -12,3 +30,9 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 
 python3 manage.py collectstatic --noinput
+
+echo "Starting Gunicorn and Nginx services..."
+sudo systemctl start gunicorn
+sudo systemctl start nginx
+ 
+echo "Deployment completed successfully!"
