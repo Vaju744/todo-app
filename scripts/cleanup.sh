afabb715650f5dddb0f0b24db28875432e32de62
Vaju744 <x23325666@student.ncirl.ie>
2024-12-12 06:24:44 +0000

    cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 echo "Running cleanup..."
 sudo rm -rf /home/ec2-user/todo/* || true
-sudo rm -f /home/ec2-user/todo/.pylintrc || true
-sudo rm -rf /home/ec2-user/todo/sonar-scanner-5.0.1.3006-linux || true
 echo "Cleanup completed."
-
