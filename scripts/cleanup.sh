a12d38a65690d8a9cbbb86ff191a4b1bebbed2e1
Vaju744 <x23325666@student.ncirl.ie>
2024-12-12 05:00:41 +0000

    Hope

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Running cleanup before deployment..."
+echo "Cleaning up old files..."
+sudo rm -rf /home/ec2-user/todo/*
 sudo rm -rf /home/ec2-user/todo/sonar-scanner-5.0.1.3006-linux || true
-sudo rm -rf /home/ec2-user/todo/* || true
-echo "Cleanup completed."
+echo "Cleanup completed successfully."
