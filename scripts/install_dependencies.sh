86c4e082c2224476d5cb51e1ef5c1fe05b033552
Vaju744 <x23325666@student.ncirl.ie>
2024-12-12 08:36:43 +0000

    changes

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,26 +5,21 @@ sudo systemctl stop gunicorn
 sudo systemctl stop nginx
 
 # Define the deployment directory dynamically
-DEPLOYMENT_DIR=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
+#DEPLOYMENT_DIR=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 
 # Check if the deployment directory exists
-if [ ! -d "$DEPLOYMENT_DIR" ]; then
-  echo "Deployment directory does not exist: $DEPLOYMENT_DIR"
-  exit 1
-fi
+#if [ ! -d "$DEPLOYMENT_DIR" ]; then
+ # echo "Deployment directory does not exist: $DEPLOYMENT_DIR"
+#  exit 1
+#fi
 
 # Clean up old files while preserving the scripts directory
 #echo "Cleaning up old files in /home/ec2-user/todo but preserving scripts directory..."
 #sudo find /home/ec2-user/todo -mindepth 1 ! -name "scripts" -exec rm -rf {} +
 
 # Copy new files from the deployment directory
-echo "Copying new files from $DEPLOYMENT_DIR to /home/ec2-user/todo..."
-sudo cp -r $DEPLOYMENT_DIR/* /home/ec2-user/todo
-
-# Set ownership and permissions for the new files
-echo "Setting permissions for /home/ec2-user/todo..."
-sudo chown -R ec2-user:ec2-user /home/ec2-user/todo
-sudo chmod -R 755 /home/ec2-user/todo
+#echo "Copying new files from $DEPLOYMENT_DIR to /home/ec2-user/todo..."
+#sudo cp -r $DEPLOYMENT_DIR/* /home/ec2-user/todo
 
 # Ensure necessary system packages are installed
 sudo yum update -y
@@ -34,6 +29,11 @@ cd /home/ec2-user/todo
 
 # Install Python dependencies
 pip3 install -r requirements.txt
+
+# Set ownership and permissions for the new files
+echo "Setting permissions for /home/ec2-user/todo..."
+sudo chown -R ec2-user:ec2-user /home/ec2-user/todo
+sudo chmod -R 755 /home/ec2-user/todo
 
 # Run Django database migrations
 python3 manage.py makemigrations
